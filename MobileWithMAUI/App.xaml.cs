273a2168c432fbe92bebfeab42f4e48165a27d62
Armando Costa <83301015+Costa0910@users.noreply.github.com>
2024-10-02 11:18:15 +0100

    video 6 - StackLayout

--- MobileWithMAUI/App.xaml.cs
+++ MobileWithMAUI/App.xaml.cs
@@ -11,6 +11,6 @@ public partial class App : Application
                 BarBackgroundColor = Colors.Yellow,
                 BarTextColor = Colors.Blue
             };
-        MainPage = new TabbedPageDemo();
+        MainPage = new StackLayoutDemo();
     }
 }
