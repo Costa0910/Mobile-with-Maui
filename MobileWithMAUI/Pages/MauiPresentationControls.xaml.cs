c8d0ff7bb1f1baf05977605adc511c328a9806f0
Armando Costa <83301015+Costa0910@users.noreply.github.com>
2024-10-03 14:45:55 +0100

    Video 9 - AbsoluteLayout

---
+++ MobileWithMAUI/Pages/MauiPresentationControls.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileWithMAUI.Pages;
+
+public partial class MauiPresentationControls : ContentPage
+{
+    public MauiPresentationControls()
+    {
+        InitializeComponent();
+    }
+}
