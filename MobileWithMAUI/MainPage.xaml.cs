5ae99790bfc210663db410a1c7fb721f079f7925
Armando Costa <83301015+Costa0910@users.noreply.github.com>
2024-10-07 12:34:25 +0100

    Video 15 - Data Binding I

--- MobileWithMAUI/MainPage.xaml.cs
+++ MobileWithMAUI/MainPage.xaml.cs
@@ -1,9 +1,9 @@
-﻿namespace MobileWithMAUI;
+﻿using MobileWithMAUI.Models;
+
+namespace MobileWithMAUI;
 
 public partial class MainPage : ContentPage
 {
-    int count = 0;
-
     public MainPage()
     {
         InitializeComponent();
@@ -11,13 +11,22 @@ public partial class MainPage : ContentPage
 
     private void OnCounterClicked(object sender, EventArgs e)
     {
-        count++;
+        var product = new Product
+        {
+            Name = "Product 1",
+            Price = 100.00m,
+            Stock = 10
+        };
 
-        if (count == 1)
-            CounterBtn.Text = $"Clicked {count} time";
-        else
-            CounterBtn.Text = $"Clicked {count} times";
+        var productBiding = new Binding(nameof(Models.Product.Name), source:
+            product, stringFormat: "Product: {0}");
+        var priceBinding = new Binding(nameof(Models.Product.Price), source: product,
+            stringFormat: "Price: {0:C}");
+        var stockBinding = new Binding(nameof(Models.Product.Stock), source: product,
+            stringFormat: "Stock: {0}");
 
-        SemanticScreenReader.Announce(CounterBtn.Text);
+        LabelBinding.SetBinding(Label.TextProperty, productBiding);
+        LabelPrice.SetBinding(Label.TextProperty, priceBinding);
+        LabelStock.SetBinding(Label.TextProperty, stockBinding);
     }
 }
